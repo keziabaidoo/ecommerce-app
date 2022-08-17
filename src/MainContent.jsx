12bee777bf73867d79acd00f8350a78b94a3b47d
keziabaidoo <keziabaidoo100@gmail.com>
2022-08-17 21:41:58 +0000

    text added to MainContent

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,10 +1,61 @@
 import React, { Component } from "react";
 
-class MainContent extends Component{
-    render(){
-        return<div>Hello</div>
-    }
+class MainContent extends Component {
+  state = { pageTitle: "employees", customersCount: 5 , customers:[ 
+     {id:1, name:"Sam", phone:"020335679",address:{city:"Berlin"}},
+     {id:1, name:"Jones", phone:"05435672",address:{city:"Germany"}},
+     {id:1, name:"Evans", phone:"024335678",address:{city:"London"}},
+     {id:1, name:"Paul", phone:"056335674",address:{city:"Italy"}},
+     {id:1, name:"Rocky", phone:"050335679",address:{city:"Poland"}}
+
+  ]
+};
+  render() {
+    return (
+      <div>
+        <h4 className="border-bottom m-1p-1">
+          {this.state.pageTitle}
+          <span className="badge bg-secondary">
+            {this.state.customersCount}
+          </span>
+
+          <button className="btn btn-info" onClick={this.onRefreshClick}>
+            Refresh
+          </button>
+        </h4>
+
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Customer Name</th>
+                    <th>Phone</th>
+                    <th>City</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.customers.map((cust)=>{
+                  return(
+                    <tr key={cust.id}>  
+                    <td>{cust.id}</td>
+                    <td>{cust.name}</td>
+                    <td>{cust.phone}</td>
+                    <td>{cust.address.city}</td>
+
+                </tr>
+                  );
+                })}
+                
+            </tbody>
+        </table>
+      </div>
+    )
+  }
+  onRefreshClick = () => {
+    this.setState({
+      customersCount: 10,
+    });
+  };
 }
-
-
+//Executes when the user clicks on the Refresh button
 export default MainContent;
