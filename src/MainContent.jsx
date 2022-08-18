a13918ab5ec9aeea265aa969c70cfac93fd3ea4e
keziabaidoo <keziabaidoo100@gmail.com>
2022-08-18 22:27:46 +0000

    a map statement was rendered to MainContent

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 
 class MainContent extends Component {
-  state = { pageTitle: "employees", customersCount: 5 , customers:[ 
-     {id:1, name:"Sam", phone:"020335679",address:{city:"Berlin"}},
-     {id:1, name:"Jones", phone:"05435672",address:{city:"Germany"}},
-     {id:1, name:"Evans", phone:"024335678",address:{city:"London"}},
-     {id:1, name:"Paul", phone:"056335674",address:{city:"Italy"}},
-     {id:1, name:"Rocky", phone:"050335679",address:{city:"Poland"}}
-
-  ]
-};
+  state = {
+    pageTitle: "employees",
+    customersCount: 5,
+    customers: [
+      { id: 1, name: "Sam", phone: "020335679", address: { city: "Berlin" } },
+      { id: 1, name: "Jones", phone: "05435672", address: { city: "Germany" } },
+      { id: 1, name: "Evans", phone: null, address: { city: "London" } },
+      { id: 1, name: "Paul", phone: null, address: { city: "Italy" } },
+      { id: 1, name: "Rocky", phone: "050335679", address: { city: "Poland" } },
+    ],
+  };
   render() {
     return (
       <div>
@@ -25,31 +27,35 @@ class MainContent extends Component {
         </h4>
 
         <table className="table">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Customer Name</th>
-                    <th>Phone</th>
-                    <th>City</th>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Customer Name</th>
+              <th>Phone</th>
+              <th>City</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.customers.map((cust) => {
+              return (
+                <tr key={cust.id}>
+                  <td>{cust.id}</td>
+                  <td>{cust.name}</td>
+                  <td>
+                    {cust.phone ? (
+                      cust.phone
+                    ) : (
+                      <div className="bg-warning p-2 text-center">No Phone</div>
+                    )}
+                  </td>
+                  <td>{cust.address.city}</td>
                 </tr>
-            </thead>
-            <tbody>
-                {this.state.customers.map((cust)=>{
-                  return(
-                    <tr key={cust.id}>  
-                    <td>{cust.id}</td>
-                    <td>{cust.name}</td>
-                    <td>{cust.phone}</td>
-                    <td>{cust.address.city}</td>
-
-                </tr>
-                  );
-                })}
-                
-            </tbody>
+              );
+            })}
+          </tbody>
         </table>
       </div>
-    )
+    );
   }
   onRefreshClick = () => {
     this.setState({
